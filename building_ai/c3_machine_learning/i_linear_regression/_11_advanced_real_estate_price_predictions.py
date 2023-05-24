85b7ef61dbf15b48e4be9ee3a8b353327fe4e357
oorellana95 <oorellana30.95@gmail.com>
2023-05-24 11:24:00 -0300

    add other ways of solving the exercise real estate price predictions with numpy

--- building_ai/c3_machine_learning/i_linear_regression/_11_advanced_real_estate_price_predictions.py
+++ building_ai/c3_machine_learning/i_linear_regression/_11_advanced_real_estate_price_predictions.py
@@ -10,15 +10,22 @@ import numpy as np
 # input values for three mökkis: size, size of sauna, distance to water, number of indoor bathrooms,
 # proximity of neighbors
 X = np.array([[66, 5, 15, 2, 500],
-             [21, 3, 50, 1, 100],
-             [120, 15, 5, 2, 1200]])
+              [21, 3, 50, 1, 100],
+              [120, 15, 5, 2, 1200]])
 c = np.array([3000, 200, -50, 5000, 100])  # coefficient values
 
 
 def predict(X):
     for x in X:
-        price = c.dot(x)
+        price = np.dot(c, x)  # c.dot(x)
         print(price)
 
 
 predict(X)
+
+# Other ways
+# x = np.array([[66, 5, 15, 2, 500],
+#              [21, 3, 50, 1, 100]])
+# c = np.array([3000, 200 , -50, 5000, 100])
+# print(x @ c)
+# Output: [258250 76100]
