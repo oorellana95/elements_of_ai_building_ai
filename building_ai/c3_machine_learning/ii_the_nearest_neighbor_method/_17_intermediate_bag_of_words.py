e8408af70215b8a3525a17ccfe8f3fe5e4b96f31
oorellana95 <oorellana30.95@gmail.com>
2023-05-30 19:14:38 -0300

    exercise 17 intermediate bag of words

---
+++ building_ai/c3_machine_learning/ii_the_nearest_neighbor_method/_17_intermediate_bag_of_words.py
@@ -0,0 +1,40 @@
+"""
+Exercise 17 (intermediate): Bag of Words
+
+Your task is to write a function that calculates the distances (or differences) between a pair of lines in the This
+Little Piggy rhyme.
+
+Every row in the list data represents one line in the rhyme.
+
+When you run the code, you see that the output of the whole program is a list of lists. When your function works
+correctly, each list will contain the distances between a single row and all the other rows in data.
+
+Note that the program will compare every row also with itself. In this case – when the compared rows are the same –
+their distance will be zero.
+
+You can use the function abs(x-y) to calculate the distance between numbers x and y, where x comes from list row1 and
+y comes from row2.
+
+Your program must work with any text, not only with the rhyme This Little Piggy.
+"""
+
+# this data here is the bag of words representation of This Little Piggy
+data = [[1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 3, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1]]
+
+
+def distance(row1, row2):
+    return sum([abs(x-y) for x, y in zip(row1, row2)])
+
+
+def all_pairs(data):
+    # this calls the distance function for all the two-row combinations in the data
+    # you do not need to change this
+    dist = [[distance(sent1, sent2) for sent1 in data] for sent2 in data]
+    print(dist)
+
+
+all_pairs(data)
